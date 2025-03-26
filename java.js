04626553d99478d9b43b4413f9d146cb8df0b3b4
Adnan hossen <adnanhossain665@gmail.com>
2025-03-27 01:04:11 +0600

    assignment-01

---
+++ java.js
@@ -0,0 +1,83 @@
+
+
+//Solution of 1st question:-
+
+
+
+const value = 31
+if (value % 2 === 0) {
+    console.log("Event")
+} 
+else {
+    console.log("Odd")
+}
+ 
+
+
+
+
+
+
+
+
+ //Solution of 2st question:-
+
+var number = 20
+if (number > 10) {
+    console.log("positive")
+}
+
+
+
+var number = 10
+if (number < 12) {
+    console.log("Negative")
+}
+
+
+else {
+    console.log("Zero")
+}
+
+
+
+
+
+
+
+
+
+
+ //Solution of 3st question:-
+
+function getGrade(score) {
+    if (score >= 90 && score <= 100) {
+        console.log("A");
+    } 
+
+    
+    else if (score >= 80 && score <= 89) {
+        console.log("B");
+    } 
+    
+
+    else if (score >= 70 && score <= 79) {
+        console.log("C");
+    } 
+
+    
+    else if (score >= 60 && score <= 69) {
+        console.log("D");
+    } 
+
+    
+    else {
+        console.log("F");
+    }
+}
+getGrade(70);
+
+
+
+
+
